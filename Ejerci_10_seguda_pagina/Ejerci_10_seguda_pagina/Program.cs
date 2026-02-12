1b9874fbf1481d0d68f3a8ec0731fc29d47a8e15
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-11 22:22:43 -0400

    Ejercio 10 y 11 (El 11 me da error, necesito que me lo explique)

---
+++ Ejerci_10_seguda_pagina/Ejerci_10_seguda_pagina/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> lista = new List<int>();
+        bool encontrado = false;
+
+        Console.Write("¿Cuántos números desea introducir? ");
+        int cantidad = Convert.ToInt32(Console.ReadLine());
+
+        
+        for (int i = 1; i <= cantidad; i++)
+        {
+            Console.Write("Ingrese el número " + i + ": ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            lista.Add(numero);
+        }
+
+       
+        Console.Write("\nIngrese el número que desea buscar: ");
+        int buscar = Convert.ToInt32(Console.ReadLine());
+
+       
+        for (int i = 0; i < lista.Count; i++)
+        {
+            if (lista[i] == buscar)
+            {
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (encontrado)
+            Console.WriteLine("El número está presente en la lista.");
+        else
+            Console.WriteLine("El número NO está presente en la lista.");
+    }
+}
---
+++ Ejerci_11_seguda_pagina/Ejerci_11_seguda_pagina/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> lista = new List<int>();
+        int posicion = -1;
+
+        Console.Write("¿Cuántos números desea introducir? ");
+        int cantidad = Convert.ToInt32(Console.ReadLine());
+
+        
+        for (int i = 1; i <= cantidad; i++)
+        {
+            Console.Write("Ingrese el número " + i + ": ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            lista.Add(numero);
+        }
+
+        
+        Console.Write("\nIngrese el número que desea buscar: ");
+        int buscar = Convert.ToInt32(Console.ReadLine());
+
+        
+       
+        {
+            if (lista[i] == buscar)
+            {
+                posicion = i;
+                break;
+            }
+        }
+
+        if (posicion != -1)
+            Console.WriteLine("El número se encuentra en la posición: " + posicion);
+        else
+            Console.WriteLine("El número no se encuentra en la lista.");
+    }
+}
