df46e30286e9244835d1d2b517e1473cbfd14efa
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-11 21:45:09 -0400

    subiendo 2 y 3

---
+++ Ejerci_2_seguda_pagina/Ejerci_2_seguda_pagina/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese una palabra: ");
+        string palabra = Console.ReadLine();
+
+        string invertida = "";
+
+        
+        for (int i = palabra.Length - 1; i >= 0; i--)
+        {
+            invertida += palabra[i];
+        }
+
+        
+        if (palabra.ToLower() == invertida.ToLower())
+        {
+            Console.WriteLine("La palabra es palíndroma.");
+        }
+        else
+        {
+            Console.WriteLine("La palabra NO es palíndroma.");
+        }
+    }
+}
---
+++ Ejerci_3_seguda_pagina/Ejerci_3_seguda_pagina/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese una oración: ");
+        string oracion = Console.ReadLine();
+
+        
+        oracion = oracion.ToLower();
+
+      
+        string limpia = "";
+
+        foreach (char c in oracion)
+        {
+            if (char.IsLetterOrDigit(c))
+            {
+                limpia += c;
+            }
+        }
+
+        bool esPalindroma = true;
+
+        for (int i = 0; i < limpia.Length / 2; i++)
+        {
+            if (limpia[i] != limpia[limpia.Length - 1 - i])
+            {
+                esPalindroma = false;
+                break;
+            }
+        }
+
+        if (esPalindroma)
+            Console.WriteLine("La oración es palíndroma.");
+        else
+            Console.WriteLine("La oración NO es palíndroma.");
+    }
+}
