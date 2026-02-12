9f8bff498267742103102389bde7ecf8b4693a3d
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-11 20:34:18 -0400

    Subiendo el primero

---
+++ Ejerci_1/Ejerci_1/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int numero;
+        int contador = 0;
+
+        Console.WriteLine("Ingrese un numero: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= numero; i++)
+        {
+            if (i % 2 == 0)
+            {
+                contador = contador + 1;
+            }
+        }
+
+        Console.WriteLine("La cantidad de numeros pares entre 1 y " + numero + " es: " + contador);
+    }
+}
