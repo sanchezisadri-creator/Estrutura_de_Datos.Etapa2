5e9f284822cbc863a81ff329952d9ea9b0f58524
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-11 20:48:37 -0400

    El 3 y 4

---
+++ Ejerci_3/Ejerci_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+        int suma = 0;
+        double media;
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine("Ingrese el numero " + i + ": ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            suma = suma + numero;
+        }
+
+        media = (double)suma / 5;
+
+        Console.WriteLine("La media aritmetica es: " + media);
+    }
+}
---
+++ Ejerci_4/Ejerci_4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1;
+        int num2;
+        int mcm;
+
+        Console.WriteLine("Ingrese el primer numero: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo numero: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        // Empezamos desde el mayor
+        if (num1 > num2)
+        {
+            mcm = num1;
+        }
+        else
+        {
+            mcm = num2;
+        }
+
+        while (true)
+        {
+            if (mcm % num1 == 0 && mcm % num2 == 0)
+            {
+                break;
+            }
+
+            mcm = mcm + 1;
+        }
+
+        Console.WriteLine("El MCM es: " + mcm);
+    }
+}
