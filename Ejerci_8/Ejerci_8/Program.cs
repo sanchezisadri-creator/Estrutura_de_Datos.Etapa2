f24a6479e458e33769974565d5581f9db00cc92b
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-11 21:25:26 -0400

    El 8

---
+++ Ejerci_8/Ejerci_8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+        int positivos = 0;
+        int negativos = 0;
+        int ceros = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine("Ingrese el número " + i + ": ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero > 0)
+            {
+                positivos++;
+            }
+            else if (numero < 0)
+            {
+                negativos++;
+            }
+            else
+            {
+                ceros++;
+            }
+        }
+
+        Console.WriteLine("Cantidad de positivos: " + positivos);
+        Console.WriteLine("Cantidad de negativos: " + negativos);
+        Console.WriteLine("Cantidad de ceros: " + ceros);
+    }
+}
