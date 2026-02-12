ab3cfcaa409ce5e80c972d80309b28ef49d95998
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-11 20:38:47 -0400

    SUbiendo el dos .Arranco el estres

---
+++ Ejerci_2/Ejerci_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+        int i = 1;
+        int suma = 0;
+
+        Console.WriteLine("Ingrese un numero: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        while (i < numero)
+        {
+            if (numero % i == 0)
+            {
+                suma = suma + i;
+            }
+
+            i = i + 1;
+        }
+
+        if (suma == numero && numero > 0)
+        {
+            Console.WriteLine("El numero es perfecto.");
+        }
+        else
+        {
+            Console.WriteLine("El numero no es perfecto.");
+        }
+    }
+}
